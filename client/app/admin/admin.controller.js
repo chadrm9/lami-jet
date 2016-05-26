8d4ef056db88adbca1fa42850ebe133f809b0272
chadrm9 <chad.r.milburn@gmail.com>
2016-05-26 13:02:28 -0400

    Add admin edit user

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -3,16 +3,66 @@
 (function() {
 
 class AdminController {
-  constructor(User, alertService) {
+  constructor(User, Auth, $state, alertService) {
     // Use the User $resource to fetch all users
-    this.users = User.query();
+    this.User = User;
+    this.users = this.User.query();
+    this.user = {};
+
+    this.Auth = Auth;
+    this.$state = $state;
+
+    this.edit = false;
     this.alertService = alertService;
   }
 
   deleteUser(user) {
     user.$remove();
     this.users.splice(this.users.indexOf(user), 1);
-    this.alertService.add('success', 'User deleted!', 3000);
+    this.alertService.add('success', 'User deleted!', 5000);
+  }
+
+  saveUser(form) {
+    this.submitted = true;
+
+    if (form.$valid) {
+      this.Auth.updateUser(this.user)
+      .then(() => {
+
+        // User details updated, refresh list
+        this.users = this.User.query();
+        this.edit = false;
+        this.alertService.add('success', 'User updated!', 5000);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    }
+  }
+
+  savePassword(form) {
+    this.pwSubmitted = true;
+
+    if (form.$valid) {
+      this.Auth.updateUser(this.user)
+      .then(() => {
+
+        // User password updated, refresh list
+        this.users = this.User.query();
+        this.edit = false;
+        this.alertService.add('success', 'Password updated!', 5000);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    }
+  }
+
+  resetEdit() {
+    this.users = this.User.query();
+    this.edit = false;
+    this.submitted = false;
+    this.pwSubmitted = false;
   }
 }
 
